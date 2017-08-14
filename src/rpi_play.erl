41bbfaeae09fdf603d6abdf756136599fcca1246
Chris Maguire <cwmaguire@gmail.com>
2017-08-13 21:59:26 -0700

    Add API to set commands to epigpio
    For now this is going to look like a copy of the epigpio API. This is
    just to get started. Later I plan on having this create programs that use
    the epigpio API.

--- src/rpi_play.erl
+++ src/rpi_play.erl
@@ -7,6 +7,9 @@
 
 %% API
 -export([start_link/0]).
+-export([set_output_mode/0]).
+-export([power_on/0]).
+-export([power_off/0]).
 
 %% gen_server callbacks
 -export([init/1,
@@ -16,22 +19,42 @@
          terminate/2,
          code_change/3]).
 
--record(state, {}).
+-record(state, {epigpio :: pid()}).
+
+set_output_mode() ->
+    gen_server:cast(?MODULE, set_output_mode).
+
+power_on() ->
+    gen_server:cast(?MODULE, power_on).
+
+power_off() ->
+    gen_server:cast(?MODULE, power_off).
 
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-    {ok, #state{}}.
+    {ok, Epigpio} = epigpio_api:start_pigpio_client(self()),
+    {ok, #state{epigpio = Epigpio}}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
 
+handle_cast(set_output_mode, State = #state{epigpio = Epigpio}) ->
+    epigpio:set_mode(Epigpio, 4, 1),
+    {noreply, State};
+handle_cast(power_on, State = #state{epigpio = Epigpio}) ->
+    epigpio:write(Epigpio, 4, 1),
+    {noreply, State};
+handle_cast(power_off, State = #state{epigpio = Epigpio}) ->
+    epigpio:write(Epigpio, 4, 0),
+    {noreply, State};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info(_Info, State) ->
+handle_info(Msg, State) ->
+    io:format("Message from epigpio: ~p~n", [Msg]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
