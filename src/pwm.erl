22f2e9ff940afbd6e68526c540e22a946fdfc0f1
Chris Maguire <cwmaguire@gmail.com>
2017-08-19 18:43:57 -0700

    Cycle different colours in an RGB with PWM
    Turns out PWM through pigpio just takes a number from 0 to 255 where 0
    is ... 0 voltage (?) and 255 is 100% voltage (?). Or something. Anyways,
    it works and the different aspects of the lights come on.

--- src/pwm.erl
+++ src/pwm.erl
@@ -2,6 +2,40 @@
 
 -export([go/0]).
 
+-define(RED, 20).
+-define(GREEN, 21).
+-define(BLUE, 19).
+
 go() ->
-    rpi_play:set_output_mode(10),
-    rpi_play:pwm(10, 10000).
+    Red = spawn(fun() -> cycle(?RED, 5, 0) end),
+    Green = spawn(fun() -> cycle(?GREEN, 4, 0) end),
+    Blue = spawn(fun() -> cycle(?BLUE, 3, 0) end),
+    {stop_fun(Red, Green, Blue), {Red, Green, Blue}}.
+
+stop_fun(Red, Green, Blue) ->
+    fun() ->
+         Red ! stop,
+         Green ! stop,
+         Blue ! stop
+    end.
+
+cycle(Pin, Step, TooHigh) when TooHigh + Step > 255 ->
+    cycle(Pin, Step, 0);
+cycle(Pin, Step, Current) ->
+    rpi_play:pwm(Pin, Step),
+    ShouldContinue =
+    receive
+        stop ->
+            false;
+        Msg ->
+            io:format("~p (pin ~p) receive message: ~p~n", [self(), Pin, Msg]),
+            true
+    after 0 ->
+        true
+    end,
+    case ShouldContinue of
+        false ->
+            okay;
+        _ ->
+            cycle(Pin, Step, Current + Step)
+    end.
